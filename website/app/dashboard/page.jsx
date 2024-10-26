937aed4d0013ac1400fd5ddc706f4eaa960417b5
Kush_Puro <kp4010@srmist.edu.in>
2024-10-26 17:58:50 +0530

    Update page.jsx

--- website/app/dashboard/page.jsx
+++ website/app/dashboard/page.jsx
@@ -31,35 +31,10 @@ export default function PrivatePage() {
 
   // Sample article data
   const latestNews = [
-    {
-      title: "Thyssenkrup loses legal fight!!",
-      imageUrl: "https://steelindustry.news/wp-content/uploads/2024/09/imports3-750x375.jpg",
-      link: "https://www.moneycontrol.com/news/business/companies/thyssenkrupp-loses-legal-fight-against-eu-antitrust-veto-of-tata-steel-jv-12835518.html",
-    },
-    {
-      title: "Indian steel majors best placed steel producers",
-      imageUrl: "https://bsmedia.business-standard.com/_media/bs/img/article/2023-12/20/full/1703051689-4937.jpg?im=FitAndFill(826,465)",
-      link: "https://auto.economictimes.indiatimes.com/news/industry/indian-steel-majors-best-placed-producers-globally-nomura/113905805",
-    },
-    {
-      title: "Latest steel news",
-      imageUrl: "https://images.moneycontrol.com/static-mcnews/2022/10/Steel-770x433.jpg?impolicy=website&width=770&height=431",
-      link: "https://www.steelorbis.com/steel-news/latest-news/",
-    }
-  ];
+
 
   const reportsOfTheMonth = [
-    {
-      title: "January Steel Report",
-      imageUrl: "https://image.vietnamnews.vn/uploadvnnews/Article/2017/7/19/steel499105744AM.jpg",
-      link: "https://www.steelorbis.com/steel-news/latest-news/",
-    },
-    {
-      title: "Metal stocks report",
-      imageUrl: "https://images.moneycontrol.com/static-mcnews/2022/10/Steel-770x433.jpg?impolicy=website&width=770&height=431",
-      link: "https://www.financialexpress.com/market/metal-stocks-nmdc-vedanta-tata-steel-moil-hindalco-jsw-steel-surge-3-amidst-rising-iron-ore-prices-3626005/",
-    },
-  ];
+
 
   return (
     <div className="min-h-screen bg-gray-100">
@@ -113,7 +88,7 @@ export default function PrivatePage() {
 
           {/* Reports of the Month Section */}
           <div>
-            <h3 className="text-xl font-bold mb-2">Reports of the Month</h3>
+            <h3 className="text-xl font-bold mb-2"></h3>
             <div className="flex space-x-20 overflow-x-auto">
               {reportsOfTheMonth.map((report, index) => (
                 <Link key={index} href={report.link} className="flex-shrink-0" target="_blank" rel="noopener noreferrer">
